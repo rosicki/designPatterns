8b5246b192a07101d2afceba9bb627eca328a423
Patryk Rosinski <rosicki20@gmail.com>
2023-02-19 16:36:18 +0100

    singleton

--- SingletonRunner.java
+++ SingletonRunner.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class SingletonRunner {
+public class SingletonRunner {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance("COS");
+        Singleton singleton2 = Singleton.getInstance("COS2");
+        //
+        System.out.println(singleton.value);
+        System.out.println(singleton2.value);
+
+    }
 }
